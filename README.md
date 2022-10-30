ff6175ed1800f8a2a012eb4de383627535ace650
ArrBee <ri.balfourth@gmail.com>
2022-10-30 20:23:48 +0000

    Completed display function.
    JS Module pattern function created to display the board array on the HTML doc as a grid. CSS has been completed to allow this function to generate the correct design for the game.

--- README.md
+++ README.md
@@ -7,9 +7,11 @@ writing clean code with as little global code as possible, as specifically state
 My outline for completing this is as follows:
 
 -Establish a very basic HTML document with some CSS styling to accomodate the game elements. 
-
+    ::Very simple HTML & CSS rounded square with two sections, one for the board and another for
+      the other game functions (reset, etc.)
 
 -Create the JS functionality for displaying the board, and the hard-coded HTML button elements.
+    ::Initial display function created using a predetermined array.
 
 
 -Add the functionality for players to select a specific spot on the board. 
--- game.js
+++ game.js
@@ -1,17 +1,54 @@
+//Module patten Gameboard object. This creates and returns a blank board. {That can be updated?}
 const Gameboard = (() => {
+    const boardArray = [];
     const createBoard = () => {
-        const boardArray = [];
-        for (let i = 0; i < 3; i ++) {
-
+        for (let i = 0; i < 3 ; i++ ){ 
+            boardArray.push(["X", "O", "X"])
         }
+        return boardArray;
     }
 
-});
+    const updateGameboard = () => {
+
+    }
+    return {
+        createBoard,
+        boardArray,
+    };
+
+})();
+
+const gameStateChecker = ((array) => { 
 
 
-const displayController = (() => {
-
+    return
 })
+
+//
+const displayController = ((array) => {
+    const getArray = () => array;
+    const gameArea = document.querySelector(".gameArea");
+    
+    
+    const createDisplay = (array) => {
+        for (let i = 0 ; i < array.length ; i ++) {
+            for (let j = 0; j < array[i].length; j++) { 
+                const boardSection = document.createElement("div");
+                let gameArea = document.querySelector(".gameArea");
+                boardSection.innerHTML = array[i][j];
+                boardSection.setAttribute("class", "boardSection");
+                boardSection.setAttribute("id", `${i}:${j}`);
+                gameArea.appendChild(boardSection);
+            }
+        }
+        return gameArea;
+    };
+    return {
+        getArray,
+        createDisplay,
+    };
+
+})();
 
 const Player = (name, marker) => {
     const getName = () => name;
@@ -22,3 +59,5 @@ const Player = (name, marker) => {
 }
 
 
+Gameboard.createBoard();
+displayController.createDisplay(Gameboard.boardArray);
